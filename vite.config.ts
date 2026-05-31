523940c40543b4cd4cb08e4a8cb961a03732d7db
censevenn <ohubarenko.de@gmail.com>
2026-06-01 01:48:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // Указываем правильный базовый путь для GitHub Pages
-  base: "/ctest-wizard/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  // Базовый путь для деплоя на GitHub Pages (имя вашего репозитория)
+  base: mode === 'production' ? '/ctest-wizard/' : '/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  }
-});
+}));
