052cd54997d3711d34c3a8b6664b05311f6e3df5
censevenn <ohubarenko.de@gmail.com>
2026-06-01 01:27:55 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/context/ThemeProvider";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
-const routerBasename = (() => {
-  const base = import.meta.env.BASE_URL.replace(/\/$/, "");
-  if (base) return base;
-  return typeof window !== "undefined" && window.location.pathname.startsWith("/ctest-wizard")
-    ? "/ctest-wizard"
-    : undefined;
-})();
-
-const App = () => (
-  <ThemeProvider>
-    <Sonner />
-    <BrowserRouter basename={routerBasename || undefined}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>
-);
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Sonner />
+      {/* Четко прописываем basename репозитория для react-router */}
+      <BrowserRouter basename="/ctest-wizard">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
 export default App;
