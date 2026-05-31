d2cda5ef2d8620b61034df4f928d97b72c5d4355
censevenn <ohubarenko.de@gmail.com>
2026-06-01 00:26:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // Вернули swc-плагин, который точно есть в твоём проекте
 import path from "path";
 
 export default defineConfig({
-  base: "/ctest-wizard/", // Принудительно задаем путь для GitHub Pages
+  base: "/ctest-wizard/", // Наш принудительный путь для GitHub Pages остаётся на месте
   server: {
     host: "::",
     port: 8080,
